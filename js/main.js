f8e01541aa3d196cc53bccbe841746468b6e887b
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-12-18 16:44:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
-if (!pass1) 
-history.go(-1);
-if (pass1.toLowerCase() == "letmein") {
-alert('You Got it Right!');
-window.open('www.wikihow.com');
-break;
-} 
-testV+=1;
-var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-}
-if (pass1.toLowerCase()!="password" & testV ==3) 
-history.go(-1);
-return " ";
-} 
+/* function passWord() {
+  
+  var testV = 1;
+  var pass1 = prompt('Please Enter Your Password',' ');
+  var body;
+  area = document.getElementByClassName("area");
+  area.style.display = "none"
+  while (testV < 3) {
+    if (!pass1) 
+       history.go(-1); 
+    if (pass1.toLowerCase() == "unblockme") {
+      alert('You Got it Right!');
+      window.open('/Unblock/');
+      break;
+    } 
+    testV+=1;
+    var pass1 = 
+        prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+  }
+  if (pass1.toLowerCase()!="password" & testV ==3) 
+    alert('Access Denied - Please try again in 24 hours');
+    history.go(-1);
+    return " ";
+} */
 
 function openSite(evt, siteName) {
   // Declare all variables
